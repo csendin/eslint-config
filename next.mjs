c33746c6a934181f12c9878f10e302434ca35fd3
Caio Sendin <caiosendindev.com>
2025-01-19 19:21:28 -0300

    feat: update config

--- next.mjs
+++ next.mjs
@@ -63,6 +63,7 @@ export default [
             'jsx-a11y/aria-unsupported-elements': 'warn',
             'jsx-a11y/role-has-required-aria-props': 'warn',
             'jsx-a11y/role-supports-aria-props': 'warn',
+            'react/react-in-jsx-scope': 'off',
             'react/no-unknown-property': 'error',
         },
     },
